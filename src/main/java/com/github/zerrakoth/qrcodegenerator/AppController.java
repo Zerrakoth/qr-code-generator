24ac0795859f7d2b5a787c5fa31ed43870dbdc4f
Zerrakoth <106001768+Zerrakoth@users.noreply.github.com>
2023-12-24 17:44:52 -0500

    Wrote app functionality
    Wrote out code in the onButtonClick method that makes the app work when the user clicks on the Generate button. It calls the QR code API and grabs the link the user has entered into the TextField box, then saves the .png in a custom size temporarily. After it applies that image to the program itself, it deletes the temporary image from the directory to save space. Error catching is added as well for when an invalud input is entered, or if something breaks when trying to call the API.

--- src/main/java/com/github/zerrakoth/qrcodegenerator/AppController.java
+++ src/main/java/com/github/zerrakoth/qrcodegenerator/AppController.java
@@ -5,9 +5,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class AppController
 {
@@ -41,6 +49,63 @@ public class AppController
     @FXML
     private void onButtonClick()
     {
+        // If the error text is already visible when the button is clicked, make it invisible.
+        if (errorText.isVisible())
+        {
+            errorText.setVisible(false);
+        }
+
+        // When the program is opened or ran, don't have focus on the TextField so the user can see the promptText.
+        button.requestFocus();
+
+        String userInput = getUserInput();
+        userInput = userInput.trim().toLowerCase();
+
+        // Check to verify the user entered a proper URL in the TextField box.
+        if (userInput.startsWith("http://") || userInput.startsWith("https://"))
+        {
+            String apiURL = "https://api.qrserver.com/v1/create-qr-code/?data=" + userInput + "&size=250x250";
+
+            try
+            {
+                // Call the API.
+                URL url = new URL(apiURL);
+                InputStream inputStream = url.openStream();
+
+                // Create a temporary file that contains the .png file from the API.
+                Path tempImage = Files.createTempFile("tempQRCode", ".png");
+                Files.copy(inputStream, tempImage, StandardCopyOption.REPLACE_EXISTING);
+
+                inputStream.close();
+
+                Image newImageObj = new Image(tempImage.toUri().toString());
+                image.setImage(newImageObj);
+
+                // Delete the temporary image file.
+                Files.deleteIfExists(tempImage);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+                System.out.println("Possible issue with URL or API.");
+
+                // Remove QR Code image if there is one on screen.
+                image.setImage(null);
+
+                errorText.setVisible(true);
+                errorText.setText("ERROR - Please try again");
+            }
+        }
+        else
+        {
+            System.out.println("Entered an invalid URL, most likely didn't start with http:// or https://");
+
+            // Remove QR Code image if there is one on screen.
+            image.setImage(null);
+
+            errorText.setVisible(true);
+            errorText.setText("ERROR - Invalid URL entered.");
+        }
     }
 
     public String getUserInput()
